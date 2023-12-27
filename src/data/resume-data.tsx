13cd80ca15a965e5c7261e1a091dcce522514bb2
David Chung <vlongc@gmail.com>
2023-12-27 02:00:49 -0500

    update descriptions

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -28,7 +28,7 @@ export const RESUME_DATA = {
     "Full Stack Mobile Engineer focused on building products with extra attention to details",
   summary:
     "As a Full Stack Mobile Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with Android, Kotlin, TypeScript, React, Node.js. I have over 5 years of experience in working remotely with companies all around the world.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
+  avatarUrl: "https://pbs.twimg.com/profile_images/1713322706609827840/r7lsuZUg_400x400.jpg",
   personalWebsiteUrl: "https://dchung.dev",
   contact: {
     email: "dvlchung@gmail.com",
@@ -69,7 +69,7 @@ export const RESUME_DATA = {
       start: "2021",
       end: "2023",
       description:
-        "Founded Verdict MMA and raised a seed round to create an MMA social platform with a sports betting product. Implemented new features and tech stack from inception to finish. Led team on improving the way developers ship code. Technologies: Android, Kotlin, node.js, React, TypeScript, MySQL",
+        "Founded Verdict MMA and raised a seed round to create an MMA social platform with a sports betting product. Implemented new features and tech stack from inception to finish while leading the team on improving the way developers ship code.",
     },
     {
       company: "FantasyPros",
@@ -80,7 +80,7 @@ export const RESUME_DATA = {
       start: "2018",
       end: "2021",
       description:
-        "Created and maintained 6 Android mobile apps for fantasy sports users. Increased the average app star rating from 4.0 to 4.6. Double the revenue apps bringing one of them to the top 10 grossing app in the sports category. Converted the entire codebase to 100% kotlin. Built tools on Android for companies like ESPN, Yahoo, Athletic, NBA. Technologies: Android, Kotlin, gradle",
+        "Created and maintained 6 Android mobile apps for fantasy sports users. Increased the average app star rating from 4.1 to 4.7 and doubled the app's MMR bringing it to one of the top 10 grossing app in the sports category. Converted the entire codebase to 100% kotlin. Built tools on Android for companies like ESPN, Yahoo, Athletic, NBA.",
     },
     {
       company: "COX Automotive",
@@ -91,7 +91,7 @@ export const RESUME_DATA = {
       start: "2016",
       end: "2018",
       description:
-        "Created ETL services daily to gather data on all financial lease and loan offers in the country for all cars. Create financial software for dealership including Mazda, Volvo, Hyundai, Porsche. Technologies: C#, SQL, BASH",
+        "Created ETL services daily to gather data on all financial lease and loan offers in the country for all cars. Create financial software for dealership including Mazda, Volvo, Hyundai, Porsche.",
     },
     {
       company: "Yellow Pages Next Home",
@@ -101,15 +101,16 @@ export const RESUME_DATA = {
       logo: NSNLogo,
       start: "2016",
       end: "2014",
-      description: "Built a secure RESTful API using .NET with spatial data for real estate listings. Deployed an ETL solution to integrate over 200,000 resale and rental listings daily. Automate services for email templating and daily statisitcal reporting. Technologies: C#, .NET, SQL",
+      description: "Built a secure RESTful API using .NET with spatial data for real estate listings. Deployed an ETL solution to integrate over 200,000 resale and rental listings daily. Automate services for email templating and daily statisitcal reporting.",
     },
   ],
   skills: [
     "Android",
+    "Java",
     "Kotlin",
     "JavaScript",
     "TypeScript",
-    "React/Next.js",
+    "React",
     "Node.js",
     "SQL",
     "GO",
@@ -121,10 +122,7 @@ export const RESUME_DATA = {
         "Production",
         "TypeScript",
         "Next.js",
-        "Node.js",
-        "CI/CD",
         "React",
-        "Redis",
       ],
       description: "A platform for MMA fans to share their predictions, score rounds and compete with friends",
       logo: ConsultlyLogo,
@@ -135,9 +133,9 @@ export const RESUME_DATA = {
     },
     {
       title: "Verdict MMA - Android App",
-      techStack: ["Production", "Android", "Kotlin", "Coroutines", "Dagger", "CI/CD"],
+      techStack: ["Production", "Kotlin", "Coroutines", "CI/CD"],
       description:
-        "Android version of the Verdict MMA",
+        "Android app of Verdict MMA, a platform for MMA fans to share their predictions, score rounds and compete with friends",
       logo: JarockiMeLogo,
       link: {
         label: "verdictmma.com",
@@ -146,9 +144,9 @@ export const RESUME_DATA = {
     },
     {
       title: "Fantasy Football My Playbook - Android App",
-      techStack: ["Production", "Android", "Kotlin", "RxJava", "Dagger", "CI/CD", "Mockito"],
+      techStack: ["Production", "Kotlin", "RxJava", "Mockito"],
       description:
-        "My Playbook is a suite of powerful tools to help you make better decisions, manage your lineups, and win your Fantasy Football leagues!",
+        "My Playbook is a suite of powerful tools to help you make better decisions, and win your Fantasy Football leagues!",
       logo: Minimal,
       link: {
         label: "My Playbook",
@@ -157,9 +155,9 @@ export const RESUME_DATA = {
     },
     {
       title: "Fantasy Baseball My Playbook - Android App",
-      techStack: ["Production", "Android", "Kotlin"],
+      techStack: ["Production", "Kotlin"],
       description:
-        "My Playbook is a suite of powerful tools to help you make better decisions, manage your lineups, and win your Fantasy Baseball leagues!",
+        "My Playbook is a suite of powerful tools to help you make better decisions, and win your Fantasy Baseball leagues!",
       logo: BarepapersLogo,
       link: {
         label: "My Playbook Baseball",
